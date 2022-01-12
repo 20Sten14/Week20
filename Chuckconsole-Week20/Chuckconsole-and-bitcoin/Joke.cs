bcc6890a951109f736c54554adf8af0f448a52b5
Opilane TTHK <opilane@tthk.ee>
2022-01-12 16:07:45 +0200

    Tööd

---
+++ Chuckconsole-Week20/Chuckconsole-and-bitcoin/Joke.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Chuckconsole_and_bitcoin
+{
+    class Joke
+    {
+        public value value { get; set;  }
+
+    }
+
+    public class value 
+    {
+     public string joke { get; set;  }
+    }
+}
